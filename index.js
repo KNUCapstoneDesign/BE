c86ebcae7869e4a0429c6ccec56e8f427f28b923
joo1124 <tmddjsdla@naver.com>
2025-03-25 19:11:01 +0900

    test1
    test1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const {connection, closeConnection} = require("./db");
 
