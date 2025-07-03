0cc5e5e954db0502dce770b390d2b9c422e36045
o11117 <115409472+o11117@users.noreply.github.com>
2025-07-03 17:24:25 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,15 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://port-0-planit-fe-mcmt59q6ef387a77.sel5.cloudtype.app',
+    'https://planit-fe-mcmt59q6ef387a77.sel5.cloudtype.app',
+    'https://capstonedesign-iota.vercel.app',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/api', authRoutes); // ← 여기서 /api/signup 이 완성됨
 
