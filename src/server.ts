6b9411c6a9487ca05179331bc8dab68e368c4b7b
o11117 <115409472+o11117@users.noreply.github.com>
2025-07-03 17:21:30 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 // src/server.ts
 import express from 'express';
 import authRoutes from './routes/auth.routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes); // ← 여기서 /api/signup 이 완성됨
 
